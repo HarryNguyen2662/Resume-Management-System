43ab931301852f95d82319f7c812aa717cc8ae10
Minh Le <qminh279@gmail.com>
2024-06-26 22:12:09 +0700

    setup: redux cache for api services

---
+++ client/src/services/hooks.ts
@@ -0,0 +1,6 @@
+import type { AppDispatch, RootState } from './store';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
