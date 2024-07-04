e6a3050741c7abbbf69716cd5c42ed3bc8c64882
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2024-07-04 10:57:40 +0700

    update resume schema and parsing table

--- client/src/features/resume-details/index.tsx
+++ client/src/features/resume-details/index.tsx
@@ -4,11 +4,12 @@ import { useGetResumeByIdQuery } from '@/services/apiSlice';
 
 import ResumePdfViewer from './components/resume-pdf-viewer';
 
-import ParsedJsonViewer from './components/parsed-json-viewer';
+import { ParsedJsonViewer } from './components/parsed-json-viewer';
 
 const ResumeDetails = () => {
   const { id: resumeId } = useParams();
 
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
   const { data: resume, isSuccess } = useGetResumeByIdQuery(resumeId!);
 
   if (!isSuccess) {
@@ -21,7 +22,7 @@ const ResumeDetails = () => {
   return (
     <div className="flex w-full overflow-hidden">
       <ResumePdfViewer pdfUrl={pdfUrl} />
-      <ParsedJsonViewer />
+      <ParsedJsonViewer resume={resume} />
     </div>
   );
 };
