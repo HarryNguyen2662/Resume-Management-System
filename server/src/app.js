9791b0a2ef2f73e793fe4630c4ea90f66d06694c
Nguyen Huu Phuc Toan <toantoan123toantoan@gmail.com>
2024-07-14 08:05:24 +0700

    fix backend

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ if (config.env !== 'test') {
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin || ['http://localhost:5173'].indexOf(origin) !== -1) {
+    if (!origin || ['https://coderpushcv.netlify.app'].indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
